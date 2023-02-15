de044a5683a36830ea5b468d2de0cd4846d087ee
Andyrei <106072367+Andyrei@users.noreply.github.com>
2023-02-15 01:13:05 +0100

    WIP - toDOIST

--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -2,9 +2,7 @@ import React from 'react';
 import './TodoList.css';
 
 export const TodoList = (props) => {
-  const { error, loading, onError, onLoading, onEmptyTodos, children, render } = props;
-
-  const renderFunction = children || render;
+  const { error, loading, onError, onLoading, onEmptyTodos} = props;
 
   return (
     <section>
