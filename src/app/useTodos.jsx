3b13620e2ddcf3a1867cf96c529c8e7497f985fd
Andyrei <106072367+Andyrei@users.noreply.github.com>
2023-02-15 01:26:35 +0100

    WIP - toDOIST

--- src/app/useTodos.jsx
+++ src/app/useTodos.jsx
@@ -43,6 +43,7 @@ export const useTodos = () => {
     error,
     totalTodos,
     completedTodos,
+    searchedTodos,
     completeTodos,
     addTodos,
     deleteTodos,
--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -2,13 +2,16 @@ import React from 'react';
 import './TodoList.css';
 
 export const TodoList = (props) => {
-  const { error, loading, onError, onLoading, onEmptyTodos} = props;
+  const { error, loading, searchedTodos, onError, onLoading, onEmptyTodos, children, render } = props;
+
+  const renderFunction = children || render;
 
   return (
     <section>
       {error && onError()}
       {loading && onLoading()}
-      {!loading && onEmptyTodos()}
+      {!loading && !searchedTodos.length && onEmptyTodos()}
+      {!loading && searchedTodos.map(renderFunction)}
     </section>
   );
 };
